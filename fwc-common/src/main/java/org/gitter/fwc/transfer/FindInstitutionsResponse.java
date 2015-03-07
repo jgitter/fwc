6db6f27be158c1a93b6fc3c9dd6dceeded8da2d3
Jeff Gitter <gitter1226@gmail.com>
2015-03-07 15:48:34 -0600

    Tweaked the JAXB annotations for the SOAP response

--- fwc-common/src/main/java/org/gitter/fwc/transfer/FindInstitutionsResponse.java
+++ fwc-common/src/main/java/org/gitter/fwc/transfer/FindInstitutionsResponse.java
@@ -3,17 +3,19 @@ package org.gitter.fwc.transfer;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 @XmlRootElement(namespace = "http://fwc.gitter.org/services/")
-@XmlType(name = "FindInstitutionsResponse")
+@XmlType(name = "institutionList")
 public class FindInstitutionsResponse {
     public static final String NAME = "findInstitutionsResponse";
 
     private List<Institution> institutions;
 
-    @XmlElement(name = "institutionList")
+    @XmlElementWrapper(name = "institutionList")
+    @XmlElement(name = "institution")
     public List<Institution> getInstitutions() {
         return institutions;
     }
--- fwc-common/src/main/java/org/gitter/fwc/transfer/Institution.java
+++ fwc-common/src/main/java/org/gitter/fwc/transfer/Institution.java
@@ -2,14 +2,28 @@ package org.gitter.fwc.transfer;
 
 import java.util.Map;
 
-import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 public class Institution {
     private Map<String, String> institutionData;
 
-    @XmlElementWrapper(name = "institution")
+    @XmlElement
+    public String getAddress() {
+        return new StringBuilder().append(institutionData.get("ADDR")).append(" ")
+                .append(institutionData.get("CITY")).append(", ")
+                .append(institutionData.get("STABBR")).append(" ")
+                .append(institutionData.get("ZIP")).toString();
+    }
+
+    @XmlTransient
     public Map<String, String> getInstitutionData() {
         return institutionData;
+    }
+
+    @XmlElement
+    public String getInstitutionName() {
+        return institutionData.get("INSTNM");
     }
 
     public void setInstitutionData(Map<String, String> institutionData) {
